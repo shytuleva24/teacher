a12e2e3eb22fe22b31e16ddce44ce75e75f96d0f
Ольга Шутылева <99029912+shytuleva24@users.noreply.github.com>
2022-06-29 13:20:50 +0300

    toogleMenu

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
-// $(function(){
-//     const $menu = $('#menu')
-//     $('#iconMenu').on('click', function(event){
-//         $menu.toggle()
-//     }) 
-// }) 
 
 let hamburger = document.querySelector(".hamburger");
 let menu = document.querySelector(".menu");
+const links =  document.querySelectorAll(".menu a");
 
-hamburger.onclick = function () {
+hamburger.onclick = toggleMenu;
+
+links.forEach((element) => {
+    element.onclick = toggleMenu;
+});
+
+function toggleMenu() {
     menu.classList.toggle ("active-burger");
     hamburger.classList.toggle ("hamburger-open");
 }
-
-
-
